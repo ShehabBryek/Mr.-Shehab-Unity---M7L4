35d37836a7ff44b78ba8fbc8ac8b2d2027c084a6
ShehabBryek <shehabbryek71@gmail.com>
2025-07-05 16:17:54 +0200

    LevelUI.cs

---
+++ LevelUI.cs
@@ -0,0 +1,16 @@
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class LevelUI : MonoBehaviour
+{
+    public Text levelText;
+ 
+    private void Update()
+    {
+        levelText.text = LevelController.level.ToString();
+    }
+}
